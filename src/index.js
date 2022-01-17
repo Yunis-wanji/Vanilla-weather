0c78fcd3494c1f0212e48f4b7e988e9b7b62d5d8
Yunis-wanji <94746081+Yunis-wanji@users.noreply.github.com>
2022-01-17 20:41:28 +0300

    added the forecast

--- src/index.js
+++ src/index.js
@@ -39,6 +39,52 @@ yearElement.innerHTML = `${day} ${dateElement} ${month} ${year}`
 
 
 
+
+
+
+function formatForecast(timestamp){
+  let date = new Date( timestamp*1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response){
+  let forecast = document.querySelector("#forecast");
+  let forecastElement = response.data.daily;
+
+  let forecastHTML = `<div class="row">`;
+  
+
+  forecastElement.forEach(function (forecastDay, index) {
+    if (index < 6){
+
+    forecastHTML=
+   forecastHTML + `
+        <div class="col-2">
+            <div class="forecast-date">${formatForecast (forecastDay.dt)}</div>
+                 <img src=" https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt=""/> 
+          <p class="forecat-temp">${Math.round(forecastDay.temp.max)}°</p>
+        </div>
+    `;
+    }
+    
+  });
+
+ 
+  forecastHTML = forecastHTML + `</div>`;
+  forecast.innerHTML =forecastHTML;
+}
+
+function getForecast(coordinates){
+  let apiKey = "e22adb13ecfe9db5af2e8b64dfc0ed33"
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
+  console.log(apiUrl) 
+  axios.get(apiUrl).then(displayForecast);
+}
+
+
 function displayWeather(response) {
     document.querySelector("#city").innerHTML = response.data.name;
     document.querySelector("#temp").innerHTML = Math.round(
@@ -58,6 +104,8 @@ function displayWeather(response) {
      
     let icon = document.querySelector("#pic");
     icon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+    
+    getForecast(response.data.coord)
 
   }
   function search(city) {
